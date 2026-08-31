1c5450236eab9089ecc51163249c6ef044a43592
Aaditya Patil <aaditya24112007@gmail.com>
2026-08-31 15:29:27 +0530

    Add student profile exercise

--- basics/02_variables.py
+++ basics/02_variables.py
@@ -2,10 +2,14 @@ name = "Aaditya"
 age = 18
 college = "D N Patil College"
 branch = "E&TC"
+current_year = 1
 graduation_year = 2030
+percentage = 71.0
 
-print("My name is", name)
-print("I am", age, "years old")
-print("I study at", college)
-print("My Branch is" ,branch)
-print("My expected graduation year" , graduation_year)
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Branch:", branch)
+print("Current Year:", current_year)
+print("Graduation Year:", graduation_year)
+print("Percentage:", percentage)
