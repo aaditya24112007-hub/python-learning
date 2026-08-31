d08ef157c8e10a1193b435a51245442cc93f19d2
Aaditya Patil <aaditya24112007@gmail.com>
2026-08-31 15:16:43 +0530

    Add variables and data types exercise

---
+++ basics/02_variables.py
@@ -0,0 +1,11 @@
+name = "Aaditya"
+age = 18
+college = "D N Patil College"
+branch = "E&TC"
+graduation_year = 2030
+
+print("My name is", name)
+print("I am", age, "years old")
+print("I study at", college)
+print("My Branch is" ,branch)
+print("My expected graduation year" , graduation_year)
