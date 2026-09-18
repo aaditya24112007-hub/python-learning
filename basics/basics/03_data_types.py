4885382db2f84494a67f8a0d6327e1683d992548
Aaditya Patil <aaditya24112007@gmail.com>
2026-09-18 20:11:54 +0530

    Add data types practice
    Add a new Python script to demonstrate data types.

---
+++ basics/basics/03_data_types.py
@@ -0,0 +1,16 @@
+age = 18
+height = 5.9
+name = "Aaditya"
+is_student = True
+
+print(age)
+print(type(age))
+
+print(height)
+print(type(height))
+
+print(name)
+print(type(name))
+
+print(is_student)
+print(type(is_student))
