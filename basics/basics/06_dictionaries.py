a7bda23f3eb9502e44ab451955e662026746aa7e
Aaditya Patil <aaditya24112007@gmail.com>
2026-09-19 18:40:41 +0530

    Add dictionary and key value practice
    Added a dictionary to store personal information and print its contents.

---
+++ basics/basics/06_dictionaries.py
@@ -0,0 +1,13 @@
+data={"name": "Aaditya", "branch": "Electronics & Communication", "year": 2026, "skills": ["Python", "Machine Learning", "Data Analysis"], }
+print(data)
+
+print("Name:", data["name"])
+
+print("Branch:", data["branch"])
+
+print("Year:", data["year"])
+
+print("Skills:", data["skills"])
+
+data["College"] = "XYZ University"
+print(data)
