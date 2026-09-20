b41defc13492022ac66d1daff25274c6c50e70d9
Aaditya Patil <aaditya24112007@gmail.com>
2026-09-20 11:45:25 +0530

    Add conditional grading practice

---
+++ basics/basics/09_conditionals.py
@@ -0,0 +1,12 @@
+marks=95,80,70,60,50
+for mark in marks:
+    if mark>=90:
+        print("Grade: A")
+    elif mark>=80:
+        print("Grade: B")
+    elif mark>=70:
+        print("Grade: C")
+    elif mark>=60:
+        print("Grade: D")
+    else:
+        print("Grade: F")    
